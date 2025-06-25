1f43bbb3ad1e236059d48c82950d9cfca0c537c5
tahlsljane <lan0020@copperfieldcollege.vic.edu.au>
2025-06-25 12:29:59 +1000

    frame and text issue

--- app.py
+++ app.py
@@ -10,15 +10,19 @@ app.grid_columnconfigure((0), weight=1)
 app.grid_rowconfigure((0), weight=2)
 app.grid_rowconfigure((1), weight=4)
 app.grid_rowconfigure((2), weight=1)
-app.grid_rowconfigure((3), weight=4)
+app.grid_rowconfigure((3), weight=1)
 
 # Title
-titleText = ctk.CTkLabel(app, text="StudHealth", text_color="black", font=("Georgia", 10))
-titleText.grid(row=0, column=0, sticky="nw", padx=15, pady=15) 
+titleText = ctk.CTkLabel(app, text="StudHealth", text_color="black", font=("Georgia", 30))
+titleText.grid(row=0, column=0, sticky="nw", padx=5, pady=5) 
 
-imageFrame = ctk.CTkFrame(app, border_width=1)
-imageFrame.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+# Frame
+menuFrame = ctk.CTkFrame(app, border_width=1) 
+menuFrame.grid(row=1, column=0, sticky="nsew", padx=5, pady=5) 
+menuFrame.grid_columnconfigure(0, weight=1) 
+menuFrame.grid_rowconfigure(1, weight=1) 
 
+#btnStrategies = ctk.CTkButton(menuFrame, ) 
 
 
 app.mainloop()
